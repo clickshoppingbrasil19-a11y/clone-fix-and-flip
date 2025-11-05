9485f31669fcc544136e33dbf1a3f62c4a1ed396
clickshoppingbrasil19-a11y <clickshoppingbrasil19@gmail.com>
2025-11-05 16:52:42 -0300

    Update ColorSelectionModal.tsx

--- src/components/ColorSelectionModal.tsx
+++ src/components/ColorSelectionModal.tsx
@@ -23,7 +23,7 @@ export const ColorSelectionModal = ({ isOpen, onClose }: ColorSelectionModalProp
   const colorVariants: ColorVariant[] = [
     { key: "beige", img: productBeige, name: "Bege" },
     { key: "black", img: productBlack, name: "Preto" },
-    { key: "purple", img: productBlue, name: "Roxo" },
+    { key: "purple", img: productPurple, name: "Roxo" },
     { key: "brown", img: productBrown, name: "Marrom" },
   ];
 
